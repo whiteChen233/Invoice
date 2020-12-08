a9eeea47fa07074a6560ed0fe16acb4fc905e3b3
white <294487387@qq.com>
2020-12-08 10:49:53 +0800

    edit

--- src/main/resources/static/js/invoice.js
+++ src/main/resources/static/js/invoice.js
@@ -108,3 +108,12 @@ function moneyToCapital(n) {
         str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
     return str.replace(/零(千|百|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
 }
+
+function compute() {
+    let total = 0;
+    for (let i = 0; i < 3; i++) {
+        total += parseInt($("#item-amount-" + i).val() || '0');
+    }
+    $("#amount-lower").val(total);
+    $("#amount-upper").val(moneyToCapital(total));
+}
